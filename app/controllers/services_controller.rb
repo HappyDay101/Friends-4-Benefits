82143775ef59ff3f1188352a2d6adb3e61b87b54
Osama Suleiman <osama.suleiman97@gmail.com>
2024-01-23 21:03:28 +0900

    added the user first_name/last_name & authentication for login to allow full display of index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,6 @@
 class ServicesController < ApplicationController
   before_action :set_service, only: [:show]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @services = Service.all
