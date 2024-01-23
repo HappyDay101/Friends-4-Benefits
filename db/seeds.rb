379a01a016c2b6db016dfdfcaeffb1a3c9c78cd7
Rashad DuPaty <radupaty@gmail.com>
2024-01-23 22:39:49 +0900

    fixing logic in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,20 @@ Booking.destroy_all
 # Seed Users
 puts 'Creating 5 new users'
 5.times do
-  user = User.create(
+  user = User.create!(
+    # first_name: Faker::FunnyName.name,
     email: Faker::Internet.email,
     password: 'password'
   )
-  puts "#{user.email} has been created"
+  puts "#{user.first_name} has been created"
 end
+puts "Users are done!"
 
 # Seed Services
 japanese_cities = ['Tokyo', 'Osaka', 'Kyoto', 'Sapporo', 'Fukuoka', 'Nagoya', 'Yokohama', 'Kobe', 'Hiroshima', 'Sendai']
-
+puts 'Creating 10 new services'
 10.times do
-  Service.create(
+  service = Service.create!(
     user: User.all.sample,
     service_name: "#{['Date', 'Friend', 'Food Buddy', 'Adventure Companion'].sample}: #{Faker::Lorem.words(number: 2).join(' ')}",
     description: Faker::Lorem.paragraph,
@@ -30,20 +32,22 @@ japanese_cities = ['Tokyo', 'Osaka', 'Kyoto', 'Sapporo', 'Fukuoka', 'Nagoya', 'Y
     price: Faker::Commerce.price,
     location: japanese_cities.sample
   )
+  puts "#{service.service_name} has been created"
 end
+puts 'Services are done!'
 
-Seed Bookings
+# Seed Bookings
 puts 'Creating 15 new bookings'
 15.times do
   booking = Booking.create!(
     user: User.all.sample,
     service: Service.all.sample,
-    start_date: Faker::Date.forward(from: Date.current, days: 14),
-    end_date: Faker::Date.forward(from: Date.current, days: (15...60)),
+    start_date: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now + 3),
+    end_date: Faker::Time.between(from: DateTime.now + 3, to: DateTime.now + 6),
     comment: Faker::Lorem.paragraph_by_chars,
     status: false
   )
-  puts "#{booking.name} has been created"
+  puts "#{booking.start_date} has been created"
 end
 
 puts 'Bookings are done!'
