71f66988e2c3ba316d51854b600c0bc28d8eee3d
Rashad DuPaty <radupaty@gmail.com>
2024-01-24 19:06:25 +0900

    added index to bookings routes and edited the routes array syntax

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :services, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resources :bookings, only: [:create]  # Changed from :reservations to :bookings
+  resources :services, only: %i[index show new create edit update destroy] do
+    resources :bookings, only: %i[index create] # Changed from :reservations to :bookings
   end
 
   get '/bookings/new', to: 'bookings#new', as: 'new_booking'  # Changed from 'reservations' to 'bookings'
