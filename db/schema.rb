26dd3a751b0878cb9dcf84ce186a7d4fdf89fbd2
eduardgurchiani <edoinjp@gmail.com>
2024-01-25 21:53:47 +0900

    added specificdate

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_071421) do
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-  make-bookings-index
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "status", default: "pending"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
     t.text "comment"
     t.bigint "user_id", null: false
     t.bigint "service_id", null: false
@@ -48,8 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_071421) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
