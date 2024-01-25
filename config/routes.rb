a2d10e272387cd4d02f0a8b38b1b127112628bb5
Rashad DuPaty <radupaty@gmail.com>
2024-01-25 18:42:02 +0900

    edited booking index path to 'dashboard'

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :services, only: %i[index show new create edit update destroy] do
-    resources :bookings, only: %i[index create] # Changed from :reservations to :bookings
+    resources :bookings, only: :create # Changed from :reservations to :bookings
   end
 
+  get "dashboard", to: "bookings#index"
   get '/bookings/new', to: 'bookings#new', as: 'new_booking'  # Changed from 'reservations' to 'bookings'
 end
