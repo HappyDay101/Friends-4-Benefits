5bc1461373acf19ac3dee563979a1f796d732000
Rashad DuPaty <radupaty@gmail.com>
2024-01-25 13:32:52 +0900

    commented out attr_accessor within the user model to have the first and last names display without 'nil'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :first_name, :last_name
+  # attr_accessor :first_name, :last_name
+  # commented that out to have the first and last names show on new users
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
