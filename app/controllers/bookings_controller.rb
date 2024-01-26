c59c5f98d6a17b9d59381fa3100305004fde9a0b
Osama Suleiman <osama.suleiman97@gmail.com>
2024-01-26 22:54:15 +0900

    had to fix the booking controller create deifintion to be able to save and direct to the dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,10 +17,21 @@ class BookingsController < ApplicationController
 
   def create
     @service = Service.find(params[:service_id])
-    @booking = current_user.bookings.new(booking_params.merge(service: @service))
+
+    # Set the start_date and end_date based on pickup_date and dropoff_date
+    start_date = Date.parse(params[:booking][:pickup_date])
+    end_date = Date.parse(params[:booking][:dropoff_date])
+
+    @booking = current_user.bookings.new(
+      booking_params.merge(
+        service: @service,
+        start_date: start_date,
+        end_date: end_date
+      )
+    )
 
     if @booking.save
-      redirect_to @service, notice: 'Booking successful.'
+      redirect_to dashboard_path, notice: 'Booking successful.'
     else
       flash[:alert] = "Booking failed: #{booking_errors_message}"
       redirect_to @service
