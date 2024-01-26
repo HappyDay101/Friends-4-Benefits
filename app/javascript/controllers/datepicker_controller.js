9fd85b072d721c7abb2b4d70b323851e32cf66b4
Osama Suleiman <osama.suleiman97@gmail.com>
2024-01-26 21:08:55 +0900

    made a stimulus time controller

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,9 +3,6 @@ import flatpickr from "flatpickr"
 
 export default class extends Controller {
   connect() {
-    flatpickr(this.element, {
-      enableTime: false, // Set to true if you want to include time selection
-      dateFormat: "Y-m-d", // Adjust the date format as needed
-    });
+    flatpickr(this.element)
   }
 }
