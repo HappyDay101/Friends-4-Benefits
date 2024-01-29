569bea4e80854b76bb2d9023bbdc109ee0241dad
Rashad DuPaty <radupaty@gmail.com>
2024-01-29 17:25:59 +0900

    edited services seeds with 7 images per service. also edited the service index to only show the first of each

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,8 +1,8 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_one_attached :photo
-  
+  has_many_attached :photos
+
   validates :service_name, :description, :category, :price, :location, presence: true
   # price validation
   validates :price, numericality: { greater_than_or_equal_to: 0 }
