bfd226f7abb99d0539c98de3d2d5754546c0774b
Osama Suleiman <osama.suleiman97@gmail.com>
2024-02-01 00:02:53 +0900

    added in routes patch option to update values

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :services, only: %i[index show new create edit update destroy] do
-    resources :bookings, only: %i[create update] # Changed from :reservations to :bookings
+    resources :bookings, only: %i[create update] do
+      member do
+        patch :accept
+        patch :reject
+      end
+    end# Changed from :reservations to :bookings
   end
 
   get "dashboard", to: "bookings#index"
